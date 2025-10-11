71488069658cac56237e99267f03d519df257b21
David Vaquero <pepesan@gmail.com>
2025-10-11 10:08:41 +0200

    mejorados los ejemplos de uso

--- 31_vault/launch_tasks_with_vault.sh
+++ 31_vault/launch_tasks_with_vault.sh
@@ -1,19 +1,21 @@
 #!/bin/bash
+# Comprobacion de conexión  (hecho)
+ansible-playbook 00_ping.yaml --ask-vault-pass
 # instalación de docker
 ansible-playbook 01_docker_install.yaml --ask-vault-pass
-# creación del usuario alumno
+# creación del usuario alumno (hecho)
 ansible-playbook 02_adduser_alumno.yaml --ask-vault-pass
-# instalación de sdkman
+# instalación de sdkman (hecho)
 ansible-playbook 03_sdkman_install.yaml --ask-vault-pass
-# instalación de xrdp
+# instalación de xrdp (hecho)
 ansible-playbook 04_xrdp_install.yaml --ask-vault-pass
-# instalación de intellij
+# instalación de intellij (hecho)
 ansible-playbook 05_intellij_install.yaml --ask-vault-pass
 # instalacion de chrome y chromedriver
 ansible-playbook 06_chrome_chromedriver_install.yaml --ask-vault-pass
-# actualización de sistema
+# actualización de sistema (hecho)
 ansible-playbook 07_system_update.yaml --ask-vault-pass
-# descarga de repositorios git
+# descarga de repositorios git (hecho)
 ansible-playbook 08_download_git.yaml --ask-vault-pass
 
 # reinicio de la máquina
