55451484bbe788afe3528910f4444cfd72538987
David Vaquero <pepesan@gmail.com>
2026-01-25 17:44:51 +0100

    metida nueva tarea para copia de ficheros al servidor

--- 31_vault/launch_tasks_with_vault.sh
+++ 31_vault/launch_tasks_with_vault.sh
@@ -25,6 +25,8 @@ ansible-playbook 09_install_vscode_php.yaml --ask-vault-pass
 ansible-playbook 10_install_drupal.yaml --ask-vault-pass
 # copia de los ejemplos de drupal (hecho)
 ansible-playbook 12_copy_drupal_examples.yaml --ask-vault-pass
+# Limpia entornos drupal (hecho)
+ansible-playbook 13_clean_drupal_environment.yaml --ask-vault-pass
 # reinicio de la máquina
 ansible-playbook 20_reboot.yaml --ask-vault-pass
 
