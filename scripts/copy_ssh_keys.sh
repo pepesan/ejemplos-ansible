f76418a16132ff4c7d4e9711d9cbff75a97f81ad
David Vaquero <pepesan@gmail.com>
2026-03-15 18:14:15 +0100

    metido script de meter clave ssh a un listado de maquinas ip

---
+++ scripts/copy_ssh_keys.sh
@@ -0,0 +1,21 @@
+USER="root"   # cambia por el usuario remoto si no es root
+
+# una ip por linea
+IPS=(
+X.X.X.X
+)
+
+KEY="$HOME/.ssh/id_ed25519.pub"
+
+# Pedir contraseña por consola sin mostrarla
+read -s -p "Introduce la contraseña SSH: " PASSWORD
+
+for ip in "${IPS[@]}"; do
+    echo "Copiando clave a $ip..."
+    sshpass -p "$PASSWORD" \
+	ssh-copy-id \
+	-i "$KEY" \
+        -o StrictHostKeyChecking=no \
+        -o UserKnownHostsFile="$HOME/.ssh/known_hosts" \
+        "$USER@$ip"
+done
